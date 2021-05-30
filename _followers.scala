dbe9cecf61aa96dbf19c1f5d773d83f1956c2477
etileyli <etileyli@hotmail.com>
2021-05-30 09:51:26 +0300

    Aggregate Name Vertices

--- _followers.scala
+++ _followers.scala
@@ -6,8 +6,24 @@ names.distinct().count()
 // Result = (Long = 16605113) (Also line count)
 
 // Get distinct ProfileIDs
-val nameVertices = names.select("ProfileID").distinct()
+val nameVertices01 = names.select("ProfileID").distinct() // (select distinct applies to the ENTIRE row, not just a field.)
 
 // Count of distinct ProfileIDs (Vertices)
-nameVertices.count()
+nameVertices01.count()
 // Result = (Long = 2016596)
+
+// Get distinct ProfileIDs
+val nameVertices02 = names.select("ProfileID2").distinct()
+
+// Count of distinct ProfileID2s (Vertices)
+nameVertices02.count()
+// Result = (Long = 1719956)
+
+val nameVerticesTemp = nameVertices01.union(nameVertices02)
+
+// nameVerticesTemp.count()
+
+// Aggregate Name Vertices
+val nameVertices = nameVerticesTemp.distinct()
+nameVertices.count()
+// Result = (Long = 3071487 )
