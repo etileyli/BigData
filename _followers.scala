803c48e7d0f2b79393b5c4832be76a6bbfd7a770
etileyli <etileyli@hotmail.com>
2021-05-31 19:41:27 +0300

    BFS succesful

--- _followers.scala
+++ _followers.scala
@@ -2,7 +2,8 @@ import org.graphframes.GraphFrame
 
 // Load data
 // val names = spark.read.option("header","true").csv("data/followers")
-val names = spark.read.option("header","true").csv("data/followersSmall.csv")
+val names = spark.read.option("header","true").csv("data/followersSmallNoWhiteSpace.csv")
+//val names = spark.read.option("header","true").csv("data/followersSmall.csv")
 
 // Count of entries in file
 names.distinct().count()
@@ -82,14 +83,48 @@ subgraph.edges.show(100)
 
 // GRAPH ALGORITHMS
 
+// PageRank (It takes ~1 hour to finish)
 import org.apache.spark.sql.functions.desc
-val ranks = followersGraph.pageRank.resetProbability(0.15).maxIter(10).run()
-ranks.vertices.orderBy(desc("pagerank")).select("id", "pagerank").show(10)
+//val ranks = followersGraph.pageRank.resetProbability(0.15).maxIter(10).run()
+//ranks.vertices.orderBy(desc("pagerank")).select("id", "pagerank").show(10)
+
+import org.apache.spark.sql.functions.expr
 
 
+// Breadth-First-Search
+import org.graphframes.GraphFrame
 
+followersGraph.bfs.fromExpr("id = '601BB03C9BF3B57BB6E9DCA48218386BE08FA897'").toExpr("id = 'A77BE9FDC675901E5D01F116205253C8179C187C'").maxPathLength(2).run().show(10)
+// stationGraph.bfs.fromExpr("id = 'Townsend at 7th'").toExpr("id = 'Spear at Folsom'").maxPathLength(10).run().take(10).foreach(println)
 
-
+// TEST BFS WITH SMALLER FILE
+// val namesTempp = spark.read.option("header","true").csv("data/followersSmallNoWhiteSpaceTemp.csv")
+// namesTempp.count()
+//
+// val nameVerticesTemp01 = namesTempp.select("ProfileID").withColumnRenamed("ProfileID", "id").distinct() // (select distinct applies to the ENTIRE row, not just a field.)
+// val nameVerticesTemp02 = namesTempp.select("ProfileID2").withColumnRenamed("ProfileID2", "id").distinct()
+//
+// val nameVerticesTempTemp = nameVerticesTemp01.union(nameVerticesTemp02)
+// val nameVerticesTempp = nameVerticesTempTemp.distinct()
+// nameVerticesTempp.count()
+//
+// val timeStampEdgesTemp = namesTempp.withColumnRenamed("ProfileID", "src").withColumnRenamed("ProfileID2", "dst")
+//
+// val followersGraphTemp = GraphFrame(nameVerticesTempp, timeStampEdgesTemp)
+//
+// println(s"Total Number of Names: ${followersGraphTemp.vertices.count()}")
+// println(s"Total Number of Connections in Graph: ${followersGraphTemp.edges.count()}")
+//
+// followersGraphTemp.vertices.show()
+// followersGraphTemp.edges.show()
+//
+// import org.apache.spark.sql.functions.expr
+// val sourceNode = "emre"
+// val destNode = "gizem"
+// followersGraphTemp.bfs.fromExpr("id = 'emre'").toExpr("id = 'gizem'").maxPathLength(2).run().show(10)
+// followersGraphTemp.bfs.fromExpr("id = sourceNode").toExpr("id = destNode").maxPathLength(2).run().show(10)  // Fails to run
+// followersGraphTemp.bfs.fromExpr("id = 'A43686FA05B9706827AD2D71AC062D22BC20C352'").toExpr("id = 'F38C2741C973F0A909B346862661087AA9204369'").maxPathLength(2).run().show(10)
+// followersGraphTemp.bfs.fromExpr("id = 'B570C0733DDB00A3FEECC4E846EFAA5799454444'").toExpr("id = '3416A6F78271208D31DFAAD0B37F937B18A295E3'").maxPathLength(2).run().show(10)
 
 
 
